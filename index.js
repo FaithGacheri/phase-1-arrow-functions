e8eb67d2560689d935d72a091f4879e74acf7694
“[firstname <faith.gacheri@student.moringaschool.com>
2022-08-17 17:39:00 +0300

    Assignment Completed

---
+++ index.js
@@ -0,0 +1,22 @@
+function divide (){
+let divide = 2000 / 100
+return divide;
+}
+function square (){
+    
+    let square =  2 * 2
+    return square
+}
+
+const add = (a, b) => { 
+  
+    let add = a + b;
+    return add
+}
+
+
+
+
+
+
+
